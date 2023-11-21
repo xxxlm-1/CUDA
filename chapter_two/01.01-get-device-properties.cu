8625f252fca2b06e9ff547278288ae55c6817d7a
xxxlm-1 <132685423+xxxlm-1@users.noreply.github.com>
2023-11-21 13:30:43 +0800

    2

---
+++ chapter_two/01.01-get-device-properties.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int deviceId;
+  cudaGetDevice(&deviceId);
+
+  cudaDeviceProp props;
+  cudaGetDeviceProperties(&props, deviceId);
+
+  int computeCapabilityMajor = props.major;
+  int computeCapabilityMinor = props.minor;
+  int multiProcessorCount = props.multiProcessorCount;
+  int warpSize = props.warpSize;
+
+
+  printf("Device ID: %d\nNumber of SMs: %d\nCompute Capability Major: %d\nCompute Capability Minor: %d\nWarp Size: %d\n", deviceId, multiProcessorCount, computeCapabilityMajor, computeCapabilityMinor, warpSize);
+}
